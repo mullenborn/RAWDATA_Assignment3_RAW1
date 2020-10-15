df65aa3417f6f78afab48bbc15ac686c7f54db2f
mullenborn <nilsmullenborn@hotmail.com>
2020-10-15 15:18:08 +0200

    Init setup

---
+++ RAWDATA_Assignment3_RAW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RAWDATA_Assignment3_RAW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
