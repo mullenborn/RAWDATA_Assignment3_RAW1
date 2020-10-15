4485ae3f6f8fb04732d2bb034e76625c85b0eddf
Alex Tao <alexwoko@gmail.com>
2020-10-15 15:29:42 +0200

    Added client and server

---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Clients
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            using var client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 5000);
+
+            var stream = client.GetStream();
+
+            var data = Encoding.UTF8.GetBytes("Hello");
+
+            stream.Write(data);
+
+            data = new byte[client.ReceiveBufferSize];
+
+            var cnt = stream.Read(data);
+
+            var msg = Encoding.UTF8.GetString(data, 0, cnt);
+
+            Console.WriteLine($"Message from the server: {msg}");
+        }
+    }
+}
